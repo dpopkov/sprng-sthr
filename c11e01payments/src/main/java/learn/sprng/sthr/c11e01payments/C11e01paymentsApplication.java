cee6c85a22f5bfe068c00aa19565c1efdd6f4b9e
Denis Popkov <pkvdenis@gmail.com>
2022-09-01 09:13:17 +0300

    c11e01payments: add project.

---
+++ c11e01payments/src/main/java/learn/sprng/sthr/c11e01payments/C11e01paymentsApplication.java
@@ -0,0 +1,13 @@
+package learn.sprng.sthr.c11e01payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C11e01paymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C11e01paymentsApplication.class, args);
+    }
+
+}
---
+++ c11e01payments/src/test/java/learn/sprng/sthr/c11e01payments/C11e01paymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.sprng.sthr.c11e01payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C11e01paymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
