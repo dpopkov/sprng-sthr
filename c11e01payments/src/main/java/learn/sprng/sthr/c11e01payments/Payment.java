f3c02621e439e19af8225bc2fb2a16996d44282f
Denis Popkov <pkvdenis@gmail.com>
2022-09-01 09:54:04 +0300

    c11e01payments: implement payment.

---
+++ c11e01payments/src/main/java/learn/sprng/sthr/c11e01payments/Payment.java
@@ -0,0 +1,22 @@
+package learn.sprng.sthr.c11e01payments;
+
+public class Payment {
+    private String id;
+    private double amount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
---
+++ c11e01payments/src/main/java/learn/sprng/sthr/c11e01payments/PaymentController.java
@@ -0,0 +1,32 @@
+package learn.sprng.sthr.c11e01payments;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@RestController
+public class PaymentController {
+
+    private static final Logger logger = Logger.getLogger(PaymentController.class.getName());
+
+    @PostMapping("/payment")
+    public ResponseEntity<Payment> createPayment(
+            @RequestHeader String requestId,
+            @RequestBody Payment payment
+    ) {
+        logger.info("Received request with ID " + requestId + "; Payment Amount: " + payment.getAmount());
+
+        payment.setId(UUID.randomUUID().toString());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("requestId", requestId)
+                .body(payment);
+    }
+}
